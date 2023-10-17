dbcdb8f26bc83b7663408b800681c0244a9efe48
RostislavB74 <brn.h74@gmail.com>
2023-10-17 10:36:55 +0300

    edit

--- create_db_load_date copy.py
+++ create_db_load_date copy.py
@@ -3,17 +3,13 @@ import os
 import shutil
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-from models import Authors, Quotes
-
+# from models import Authors, Quotes
 input_directory = 'input'
 arch_directory = 'arch'
 
 
 def create_db_load_date(name_db):
-    # name_db = 'hw8'
-
     uri = f"mongodb+srv://user_python:54321@cluster0.yypw24v.mongodb.net/{name_db}?retryWrites=true&w=majority"
-
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[name_db]
 
@@ -23,22 +19,19 @@ def create_db_load_date(name_db):
                 collection_name = file.split('.')[0]
                 collection = db[collection_name]
                 with open(os.path.join(input_directory, file), "r", encoding="utf-8") as af:
-                    file_date = json.load(af)
-                    collection.insert_many(file_date)
+                    authors_date = json.load(af)
+                    collection.insert_many(authors_date)
+                
             if file.startswith("quotes"):
                 with open(os.path.join(input_directory, file), "r", encoding="utf-8") as qf:
                     file_date = json.load(qf)
-                    for quote_date in file_date:
-                        author_fullname = quote_date['author']
-                        quote_date = Authors.objects(
-                            fullname=author_fullname).first()
-                        # del quote_date['author']
-                        collection.insert_many(file_date)
-
-    # with open(os.path.join(input_directory, file), "r", encoding="utf-8") as f:
-    #         file_data = json.load(f)
-    #         collection.insert_many(file_data)
-            print(file)
+                    for quote_data in file_date:
+                        author_fullname = quote_data.get('author')
+                        author = db.authors.find_one({'fullname': author_fullname})
+                        # print(author)
+                        quote_data['author'] = author['_id']
+                        db.quotes.insert_one(quote_data) 
+ 
         shutil.move(f'input/{file}', f'arch/{file}')
 
     client.close()
@@ -46,4 +39,4 @@ def create_db_load_date(name_db):
 
 
 if __name__ == '__main__':
-    create_db_load_date("test1111")
+    create_db_load_date("test222")
